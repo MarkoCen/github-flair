ab4f277cf8b498a71d2431285d40372506221bdf
markocen <markocen@gmail.com>
2017-03-29 10:58:04 -0400

    update

--- flairStyles.js
+++ flairStyles.js
@@ -46,17 +46,320 @@
         textLink: {
             color: '#555'
         },
-        star: {
-            icon: '&#9733;',
-            color: 'yellow'
+        svgIcon: {
+            fill: '#555'
+        }
+    }
+
+    themes.dark = {
+        flair: {
+            'box-sizing': 'border-box',
+            'line-height': 'normal',
+            display: 'flex',
+            'align-items': 'center',
+            width: '290px',
+            height: '95px',
+            color: '#fff',
+            position: 'relative',
+            border: '2px solid #24292e',
+            background: '#24292e',
+            'border-radius': '3px',
+            padding: '5px 10px',
+            'font-family': '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"'
         },
-        gist: {
-            icon: '&#9998;',
-            color: 'black'
+        avatar: {
+            'text-align': 'center',
+            width: '75px',
+            height: '75px',
+            'margin-left': '5px'
         },
-        repo: {
-            icon: '&#9776;',
-            color: 'black'
+        avatarImg: {
+            width: '100%',
+            height: '100%',
+            'border-radius': '50%'
+        },
+        githubCorner: {
+            fill: '#fff',
+            color: '#24292e',
+            position: 'absolute',
+            top: 0,
+            right: 0,
+            border: 0
+        },
+        info: {
+            width: '160px',
+            'text-align': 'right',
+            'font-size': '14px'
+        },
+        name: {
+            'font-weight': 'bold',
+            'font-size': '16px'
+        },
+        textLink: {
+            color: '#fff'
+        },
+        svgIcon: {
+            fill: '#fff'
+        }
+    }
+
+    themes.gray = {
+        flair: {
+            'box-sizing': 'border-box',
+            'line-height': 'normal',
+            display: 'flex',
+            'align-items': 'center',
+            width: '290px',
+            height: '95px',
+            color: '#727272',
+            position: 'relative',
+            border: '2px solid #DADADA',
+            background: '#DADADA',
+            'border-radius': '3px',
+            padding: '5px 10px',
+            'font-family': '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"'
+        },
+        avatar: {
+            'text-align': 'center',
+            width: '75px',
+            height: '75px',
+            'margin-left': '5px'
+        },
+        avatarImg: {
+            width: '100%',
+            height: '100%',
+            'border-radius': '50%'
+        },
+        githubCorner: {
+            fill: '#727272',
+            color: '#DADADA',
+            position: 'absolute',
+            top: 0,
+            right: 0,
+            border: 0
+        },
+        info: {
+            width: '160px',
+            'text-align': 'right',
+            'font-size': '14px'
+        },
+        name: {
+            'font-weight': 'bold',
+            'font-size': '16px'
+        },
+        textLink: {
+            color: '#727272'
+        },
+        svgIcon: {
+            fill: '#727272'
+        }
+    }
+
+    themes.blue = {
+        flair: {
+            'box-sizing': 'border-box',
+            'line-height': 'normal',
+            display: 'flex',
+            'align-items': 'center',
+            width: '290px',
+            height: '95px',
+            color: '#fff',
+            position: 'relative',
+            border: '2px solid #03a9f4',
+            background: '#03a9f4',
+            'border-radius': '3px',
+            padding: '5px 10px',
+            'font-family': '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"'
+        },
+        avatar: {
+            'text-align': 'center',
+            width: '75px',
+            height: '75px',
+            'margin-left': '5px'
+        },
+        avatarImg: {
+            width: '100%',
+            height: '100%',
+            'border-radius': '50%'
+        },
+        githubCorner: {
+            fill: '#fff',
+            color: '#03a9f4',
+            position: 'absolute',
+            top: 0,
+            right: 0,
+            border: 0
+        },
+        info: {
+            width: '160px',
+            'text-align': 'right',
+            'font-size': '14px'
+        },
+        name: {
+            'font-weight': 'bold',
+            'font-size': '16px'
+        },
+        textLink: {
+            color: '#fff'
+        },
+        svgIcon: {
+            fill: '#fff'
+        }
+    }
+
+    themes.green = {
+        flair: {
+            'box-sizing': 'border-box',
+            'line-height': 'normal',
+            display: 'flex',
+            'align-items': 'center',
+            width: '290px',
+            height: '95px',
+            color: '#fff',
+            position: 'relative',
+            border: '2px solid #4caf50',
+            background: '#4caf50',
+            'border-radius': '3px',
+            padding: '5px 10px',
+            'font-family': '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"'
+        },
+        avatar: {
+            'text-align': 'center',
+            width: '75px',
+            height: '75px',
+            'margin-left': '5px'
+        },
+        avatarImg: {
+            width: '100%',
+            height: '100%',
+            'border-radius': '50%'
+        },
+        githubCorner: {
+            fill: '#fff',
+            color: '#4caf50',
+            position: 'absolute',
+            top: 0,
+            right: 0,
+            border: 0
+        },
+        info: {
+            width: '160px',
+            'text-align': 'right',
+            'font-size': '14px'
+        },
+        name: {
+            'font-weight': 'bold',
+            'font-size': '16px'
+        },
+        textLink: {
+            color: '#fff'
+        },
+        svgIcon: {
+            fill: '#fff'
+        }
+    }
+
+    themes.red = {
+        flair: {
+            'box-sizing': 'border-box',
+            'line-height': 'normal',
+            display: 'flex',
+            'align-items': 'center',
+            width: '290px',
+            height: '95px',
+            color: '#fff',
+            position: 'relative',
+            border: '2px solid #f44336',
+            background: '#f44336',
+            'border-radius': '3px',
+            padding: '5px 10px',
+            'font-family': '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"'
+        },
+        avatar: {
+            'text-align': 'center',
+            width: '75px',
+            height: '75px',
+            'margin-left': '5px'
+        },
+        avatarImg: {
+            width: '100%',
+            height: '100%',
+            'border-radius': '50%'
+        },
+        githubCorner: {
+            fill: '#fff',
+            color: '#f44336',
+            position: 'absolute',
+            top: 0,
+            right: 0,
+            border: 0
+        },
+        info: {
+            width: '160px',
+            'text-align': 'right',
+            'font-size': '14px'
+        },
+        name: {
+            'font-weight': 'bold',
+            'font-size': '16px'
+        },
+        textLink: {
+            color: '#fff'
+        },
+        svgIcon: {
+            fill: '#fff'
+        }
+    }
+
+    themes.yellow = {
+        flair: {
+            'box-sizing': 'border-box',
+            'line-height': 'normal',
+            display: 'flex',
+            'align-items': 'center',
+            width: '290px',
+            height: '95px',
+            color: '#212121',
+            position: 'relative',
+            border: '2px solid #ffeb3b',
+            background: '#ffeb3b',
+            'border-radius': '3px',
+            padding: '5px 10px',
+            'font-family': '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"'
+        },
+        avatar: {
+            'text-align': 'center',
+            width: '75px',
+            height: '75px',
+            'margin-left': '5px'
+        },
+        avatarImg: {
+            width: '100%',
+            height: '100%',
+            'border-radius': '50%'
+        },
+        githubCorner: {
+            fill: '#212121',
+            color: '#ffeb3b',
+            position: 'absolute',
+            top: 0,
+            right: 0,
+            border: 0
+        },
+        info: {
+            width: '160px',
+            'text-align': 'right',
+            'font-size': '14px'
+        },
+        name: {
+            'font-weight': 'bold',
+            'font-size': '16px'
+        },
+        textLink: {
+            color: '#212121'
+        },
+        svgIcon: {
+            fill: '#212121'
         }
     }
 
--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 (function ($, themes) {
 
     var API_BASE_URL = "https://api.github.com/";
-    var LOCATION_ICON = '<svg height="12" version="1.1" viewBox="0 0 12 16" width="12"><path fill-rule="evenodd" d="M6 0C2.69 0 0 2.5 0 5.5 0 10.02 6 16 6 16s6-5.98 6-10.5C12 2.5 9.31 0 6 0zm0 14.55C4.14 12.52 1 8.44 1 5.5 1 3.02 3.25 1 6 1c1.34 0 2.61.48 3.56 1.36.92.86 1.44 1.97 1.44 3.14 0 2.94-3.14 7.02-5 9.05zM8 5.5c0 1.11-.89 2-2 2-1.11 0-2-.89-2-2 0-1.11.89-2 2-2 1.11 0 2 .89 2 2z"></path></svg>';
-    var GITHUB_CORNER = '<svg width="30" height="30" viewBox="0 0 250 250"><path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path><path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor"></path></svg>';
+    var LOCATION_ICON = '<svg height="12" viewBox="0 0 12 16" width="12"><path fill-rule="evenodd" d="M6 0C2.69 0 0 2.5 0 5.5 0 10.02 6 16 6 16s6-5.98 6-10.5C12 2.5 9.31 0 6 0zm0 14.55C4.14 12.52 1 8.44 1 5.5 1 3.02 3.25 1 6 1c1.34 0 2.61.48 3.56 1.36.92.86 1.44 1.97 1.44 3.14 0 2.94-3.14 7.02-5 9.05zM8 5.5c0 1.11-.89 2-2 2-1.11 0-2-.89-2-2 0-1.11.89-2 2-2 1.11 0 2 .89 2 2z"></path></svg>';
+    var GITHUB_CORNER = '<svg title="GitHub Flair" width="30" height="30" viewBox="0 0 250 250"><path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path><path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor"></path></svg>';
+    var FOLLOWERS_ICON = '<svg height="12" viewBox="0 0 16 16" width="12"><path fill-rule="evenodd" d="M16 12.999c0 .439-.45 1-1 1H7.995c-.539 0-.994-.447-.995-.999H1c-.54 0-1-.561-1-1 0-2.634 3-4 3-4s.229-.409 0-1c-.841-.621-1.058-.59-1-3 .058-2.419 1.367-3 2.5-3s2.442.58 2.5 3c.058 2.41-.159 2.379-1 3-.229.59 0 1 0 1s1.549.711 2.42 2.088C9.196 9.369 10 8.999 10 8.999s.229-.409 0-1c-.841-.62-1.058-.59-1-3 .058-2.419 1.367-3 2.5-3s2.437.581 2.495 3c.059 2.41-.158 2.38-1 3-.229.59 0 1 0 1s3.005 1.366 3.005 4"></path></svg>';
+    var REPO_ICON = '<svg height="12" viewBox="0 0 12 16" width="12"><path fill-rule="evenodd" d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"></path></svg>';
+    var GIST_ICON = '<svg height="12" viewBox="0 0 12 16" width="12"><path fill-rule="evenodd" d="M7.5 5L10 7.5 7.5 10l-.75-.75L8.5 7.5 6.75 5.75 7.5 5zm-3 0L2 7.5 4.5 10l.75-.75L3.5 7.5l1.75-1.75L4.5 5zM0 13V2c0-.55.45-1 1-1h10c.55 0 1 .45 1 1v11c0 .55-.45 1-1 1H1c-.55 0-1-.45-1-1zm1 0h10V2H1v11z"></path></svg>';
     var profile = {};
 
     $('.generate').click(start);
@@ -18,8 +21,12 @@
             getProfile(data);
             getRepos(username).then(function(data){
                 getStars(data);
-                var flair = flairTemplate(themes.default);
-                $('.container').append(flair);
+                var flairs = getFlairs();
+                var flairContainer = $('.flair-container');
+                flairContainer.empty();
+                flairs.forEach(function(flair){
+                    flairContainer.append(getFlairSection(flair));
+                })
             });
         });
     }
@@ -38,6 +45,38 @@
         return $.get(url);
     }
 
+    function getFlairSection(flairNode){
+        var div = document.createElement('DIV');
+        var divFlair = document.createElement('DIV');
+        var divText = document.createElement('DIV');
+        var textArea = document.createElement('TEXTAREA');
+        div.className = "col-xs-12";
+        divFlair.className = "col-xs-6 flair-wrap";
+        divFlair.appendChild(flairNode);
+        divText.className = "col-xs-6 text-wrap";
+
+        textArea.value = $(flairNode).prop('outerHTML');
+        textArea.readOnly = 'readOnly';
+        textArea.onclick = function(){
+            this.focus();
+            this.select();
+        };
+        divText.appendChild(textArea);
+
+        div.appendChild(divFlair);
+        div.appendChild(divText);
+        return div;
+    }
+
+    function getFlairs(){
+        var themeKeys = Object.keys(themes);
+        var flairs = [];
+        for(var i = 0; i < themeKeys.length; i++){
+            flairs.push(flairTemplate(themes[themeKeys[i]]));
+        }
+        return flairs;
+    }
+
     function getProfile(userData){
         profile.avatar = userData['avatar_url'];
         profile.link = userData['html_url'];
@@ -46,9 +85,9 @@
         profile.blog = userData['blog'];
         profile.company = userData['company'];
 
-        profile.followers = userData['followers'];
+        profile.followers = truncateNum(userData['followers']);
         profile.followerUrl = userData['followers_url'];
-        profile.followings = userData['followings'];
+        profile.followings = truncateNum(userData['followings']);
         profile.followingUrl = userData['following_url'];
 
         profile.gists = truncateNum(userData['public_gists']);
@@ -125,11 +164,12 @@
     function infoTemplate(theme){
         var div = document.createElement('DIV');
         div.className = 'info';
+        div.style = styleString(theme['info']);
 
         var nameNode = nameTemplate(profile.username, profile.link, theme);
-        var metaNode = metaTemplate(profile.stars, profile.repos, profile.gists);
-        var locNode = locationTemplate(profile.location);
-        var blogNode = blogTemplate(profile.blog);
+        var metaNode = metaTemplate(profile.followers, profile.repos, profile.gists, theme);
+        var locNode = locationTemplate(profile.location, theme);
+        var blogNode = blogTemplate(profile.blog, theme);
 
         div.appendChild(nameNode);
         div.appendChild(metaNode);
@@ -152,18 +192,22 @@
         return div;
     }
 
-    function metaTemplate(starCount, repoCount, gistCount){
+    function metaTemplate(followerCount, repoCount, gistCount, theme){
         var div = document.createElement('DIV');
         div.className = 'meta';
         var starSpan = document.createElement('SPAN');
         var repoSpan = document.createElement('SPAN');
         var gistSpan = document.createElement('SPAN');
 
-        starSpan.innerHTML = '&#9733;' + starCount + '&nbsp;&nbsp;';
-        repoSpan.innerHTML = '&#9776; ' + repoCount + '&nbsp;&nbsp;';
-        gistSpan.innerHTML = '&#9998; ' + gistCount;
+        starSpan.innerHTML = FOLLOWERS_ICON + ' ' + followerCount + '&nbsp;&nbsp;';
+        repoSpan.innerHTML = REPO_ICON + ' ' + repoCount + '&nbsp;&nbsp;';
+        gistSpan.innerHTML = GIST_ICON + ' ' + gistCount;
 
-        starSpan.title = 'Total Public Stargazers';
+        starSpan.firstChild.style = styleString(theme['svgIcon']);
+        repoSpan.firstChild.style = styleString(theme['svgIcon']);
+        gistSpan.firstChild.style = styleString(theme['svgIcon']);
+
+        starSpan.title = 'Followers';
         repoSpan.title = 'Total Public Repositories';
         gistSpan.title = 'Total Public Gists';
 
@@ -174,7 +218,7 @@
         return div;
     }
     
-    function blogTemplate(url) {
+    function blogTemplate(url, theme) {
         if(!url) return;
         var div = document.createElement('DIV');
         var a = document.createElement('A');
@@ -182,16 +226,19 @@
         a.href = url;
         a.target = "_blank";
         a.innerHTML = 'Blog / Website';
+        a.style = styleString(theme['textLink']);
         div.appendChild(a);
         return div;
     }
 
-    function locationTemplate(loc){
+    function locationTemplate(loc, theme){
         if(!loc) return;
         var div = document.createElement('DIV');
         var span = document.createElement('SPAN');
         div.className = 'location';
         div.insertAdjacentHTML('afterbegin', LOCATION_ICON);
+        var svg = div.firstChild;
+        svg.style = styleString(theme['svgIcon']);
         span.innerHTML = '&nbsp;' + loc;
         div.appendChild(span);
         return div;
